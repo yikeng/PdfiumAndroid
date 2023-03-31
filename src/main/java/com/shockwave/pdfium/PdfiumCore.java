504970a761b823171f94e3a420094fe0718cb3ff
Shanks <2573475062@qq.com>
2023-03-31 12:01:49 +0800

    Migrate to AndroidX

--- src/main/java/com/shockwave/pdfium/PdfiumCore.java
+++ src/main/java/com/shockwave/pdfium/PdfiumCore.java
@@ -119,7 +119,7 @@ public class PdfiumCore {
     /** Context needed to get screen density */
     public PdfiumCore(Context ctx) {
         mCurrentDpi = ctx.getResources().getDisplayMetrics().densityDpi;
-        Log.d(TAG, "Starting PdfiumAndroid " + BuildConfig.VERSION_NAME);
+        Log.d(TAG, "Starting PdfiumAndroid ");
     }
 
     /** Create new document from file */
